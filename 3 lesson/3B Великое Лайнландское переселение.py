9038dc4683ef4fe16a5e3ba012b957efe1bd274a
dae22 <dae-22@mail.ru>
2024-11-08 12:22:10 +0300

    08/11 12:20

--- 3 lesson/3B Великое Лайнландское переселение.py
+++ 3 lesson/3B Великое Лайнландское переселение.py
@@ -1,3 +1,18 @@
 n = int(input())
-city = list(map(int, input().split()))
+city = enumerate(map(int, input().split()))
 
+stek = []
+answ = [0] * n
+for el in city:
+    if len(stek) == 0:
+        stek.append(el)
+    else:
+        while len(stek) != 0 and el[1] < stek[-1][1]:
+            nw = stek.pop(-1)
+            answ[nw[0]] = el[0]
+        stek.append(el)
+for i in range(n):
+    if answ[i] == 0:
+        print(-1, end=' ')
+    else:
+        print(answ[i], end=' ')
