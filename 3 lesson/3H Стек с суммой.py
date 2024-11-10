f073d6d3fde77e7240ca32aeff233f6d0712f8d6
dae22 <dae-22@mail.ru>
2024-11-10 18:25:43 +0300

    10/11 18:25

--- 3 lesson/3H Стек с суммой.py
+++ 3 lesson/3H Стек с суммой.py
@@ -1,7 +1,26 @@
 n = int(input())
 
-macros = {'+': 'stack.append(int(el[1]))', '-': 'print(stack.pop())', '?': 'print(sum(stack[-2:]))'}
 stack = []
+pref = [0]
 for _ in range(n):
     el = input()
-    exec(macros[el[0]])
+    if el[0] == '+':
+        stack.append(int(el[1:]))
+        pref.append(pref[len(stack)-1] + stack[-1])
+    elif el[0] == '-':
+        print(stack.pop())
+        pref.pop()
+    elif el[0] == '?':
+        k = int(el[1:])
+        print(pref[-1]-pref[-k-1])
+
+
+
+
+
+
+# macros = {'+': 'stack.append(int(el[1:]))', '-': 'print(stack.pop())', '?': 'print(sum(stack[-int(el[1:]):]))'}
+# stack = []
+# for _ in range(n):
+#     el = input()
+#     exec(macros[el[0]])
