63f6073af464ac2d8ec40a7e9dd95df3f374e9b6
dae22 <dae-22@mail.ru>
2024-11-11 14:48:53 +0300

    11/11 14:50

--- 3 lesson/3J Кровать из стульев.py
+++ 3 lesson/3J Кровать из стульев.py
@@ -1,31 +1,31 @@
 n, H = map(int, input().split())
-visota = list(map(int, input().split()))
-shirina = list(map(int, input().split()))
-chairs = sorted(zip(visota, shirina), key=lambda x: x[0])
+chairs = sorted(zip(map(int, input().split()), map(int, input().split())), key=lambda x: x[0])
 pref = [0] * n
 for i in range(1, n):
     pref[i] = chairs[i][0] - chairs[i-1][0]
 
-dis_m = 10**10
+dis_m = 10000000000
 mid = []
 dis = []
-w, r = 0, 0
-if max(shirina) >= H:
-    print(0)
-    exit()
+w, r, ld = 0, 0, 0
 for i in range(n):
-    while len(dis) and i >= dis[0][0]:
+    while ld > 0 and i >= dis[0][0]:
         dis.pop(0)
+        ld -= 1
     while w < H and r < n:
-        mid.append(chairs[r])
-        if not len(dis):
+        mid.append(chairs[r][1])
+        if ld == 0:
             dis.append([r, pref[r]])
+            ld += 1
         elif dis[-1][1] >= pref[r]:
             dis.append([r, pref[r]])
+            ld += 1
         else:
-            while len(dis) and dis[-1][1] < pref[r]:
+            while ld > 0 and dis[-1][1] < pref[r]:
                 dis.pop(-1)
+                ld -= 1
             dis.append([r, pref[r]])
+            ld += 1
         w += chairs[r][1]
         r += 1
     if w >= H:
@@ -34,6 +34,6 @@ for i in range(n):
         else:
             dis_c = dis[0][1]
         dis_m = min(dis_m, dis_c)
-    w -= mid[0][1]
+    w -= mid[0]
     mid.pop(0)
 print(dis_m)
