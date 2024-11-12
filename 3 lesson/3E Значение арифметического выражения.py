ce6bf53e269d032ba2cfc2c7b4c4c279e52f64b6
dae22 <dae-22@mail.ru>
2024-11-12 16:59:48 +0300

    12/11 17:00

--- 3 lesson/3E Значение арифметического выражения.py
+++ 3 lesson/3E Значение арифметического выражения.py
@@ -1,39 +1,40 @@
 ex = list(input().strip())
 
+flag = True
 for i in range(len(ex)):
-    while ex[i] == ' ' and ex[i] == ex[i+1]:
-        ex.pop(i+1)
-    if i == len(ex)-2:
+    while ex[i] == ' ': # and ex[i] == ex[i+1]:
+        if ex[i-1].isdigit() and ex[i+1].isdigit():
+            flag = False
+            break
+        ex.pop(i)
+    if i == len(ex)-2 or not flag:
         break
-print(ex)
+if ex[-1] in ('-', '+', '*'):
+    flag = False
+
 stack = []
 postfix = []
 znak = {'-': 1, '+': 1, '*': 2}
 open = 0
 operand = ''
-flag = True
 for i in range(len(ex)):
     if ex[i].isalpha():
         flag = False
         break
-    elif ex[i] == ' ':
-        if ex[i-1].isdigit() and ex[i+1].isdigit():
-            flag = False
-            break
     elif ex[i].isdigit():
         operand += ex[i]
         if i == len(ex)-1 or not ex[i+1].isdigit():
-            postfix.append(int(operand))
+            postfix.append(operand)
             operand = ''
     else:
         if ex[i] in ('-', '+', '*'):
-            if not ex[i-1].isdigit():
+            if not ex[i-1].isdigit() and ex[i-1] != ')':
                 flag = False
                 break
             if not stack:
                 stack.append(ex[i])
             else:
-                while stack[-1] != '(' and znak[stack[-1]] >= znak[ex[i]]:
+                while stack and stack[-1] != '(' and znak[stack[-1]] >= znak[ex[i]]:
                     postfix.append(stack.pop(-1))
                 stack.append(ex[i])
         elif ex[i] == '(':
@@ -43,33 +44,31 @@ for i in range(len(ex)):
             stack.append(ex[i])
             open += 1
         elif ex[i] == ')':
-            if ex[i-1] in ('-', '+', "*"):
+            if ex[i-1] in ('-', '+', "*") or open <= 0:
                 flag = False
                 break
             while stack and stack[-1] != '(':
                 postfix.append(stack.pop(-1))
             stack.pop(-1)
             open -= 1
+            if open < 0:
+                flag = False
+                break
         else:
             flag = False
             break
 
-
-print(postfix)
-print(stack)
-
 for el in reversed(stack):
     postfix.append(el)
 
-print(postfix)
-print(flag)
-
-
-
-
-
-
-# try:
-#     print(eval(input()))
-# except:
-#     print('WRONG')
+if flag:
+    steck = []
+    for el in postfix:
+        if el.isdigit():
+            steck.append(int(el))
+        else:
+            steck[-2] = eval(f'{steck[-2]}{el}{steck[-1]}')
+            steck.pop(-1)
+    print(steck[0])
+else:
+    print('WRONG')
--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-a, b = map(int, input().split())
-c, d = [x for x in range(1,5) if x not in (a,b)]
+
 
