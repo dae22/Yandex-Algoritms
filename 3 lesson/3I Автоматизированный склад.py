03d3ebb839360d73e502e990e9df6583bb2d7cb5
dae22 <dae-22@mail.ru>
2024-11-12 15:14:28 +0300

    12/11 15:15

--- 3 lesson/3I Автоматизированный склад.py
+++ 3 lesson/3I Автоматизированный склад.py
@@ -1,13 +1,13 @@
 n = int(input())
 a, b = map(int, input().split())
-b, c = [x for x in range(1,5) if x not in (a,b)]
+c, d = [x for x in range(1,5) if x not in (a,b)]
 rover = []
 for i in range(n):
     x, y = map(int, input().split())
     rover.append([x, y, i])
 rover.sort(key=lambda x: x[1])
 
-queue = [''] * n
+queue = [None] * n
 stack = {1: [], 2: [], 3: [], 4: []}
 t, i = 0, 0
 while True:
@@ -19,16 +19,40 @@ while True:
         if a+2 == b or b+2 == a:
             queue[stack[a][0][2]] = t
             queue[stack[b][0][2]] = t
-        elif a == b+1:
+            stack[a].pop(0)
+            stack[b].pop(0)
+        elif a == b+1 or a+3 == b:
             queue[stack[b][0][2]] = t
-        elif b == a+1:
+            stack[b].pop(0)
+        elif b == a+1 or b+3 == a:
             queue[stack[a][0][2]] = t
+            stack[a].pop(0)
     elif stack[a] and not stack[b]:
         queue[stack[a][0][2]] = t
+        stack[a].pop(0)
     elif stack[b] and not stack[a]:
         queue[stack[b][0][2]] = t
+        stack[b].pop(0)
+    elif stack[c] and stack[d]:
+        if c+2 == d or d+2 == c:
+            queue[stack[c][0][2]] = t
+            queue[stack[d][0][2]] = t
+            stack[c].pop(0)
+            stack[d].pop(0)
+        elif c == d+1 or c+3 == d:
+            queue[stack[d][0][2]] = t
+            stack[d].pop(0)
+        elif d == c+1 or d+3 == c:
+            queue[stack[c][0][2]] = t
+            stack[c].pop(0)
+    elif stack[c] and not stack[d]:
+        queue[stack[c][0][2]] = t
+        stack[c].pop(0)
+    elif stack[d] and not stack[c]:
+        queue[stack[d][0][2]] = t
+        stack[d].pop(0)
+    if not (None in queue):
+        break
 
-    if i == n: break
-
-print(rover)
-print(stack)
+for el in queue:
+    print(el)
