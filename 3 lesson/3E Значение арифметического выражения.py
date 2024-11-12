783b706bf93cf83988bf2c5fe1f41972f212a0d7
dae22 <dae-22@mail.ru>
2024-11-12 14:15:13 +0300

    12/11 14:15

--- 3 lesson/3E Значение арифметического выражения.py
+++ 3 lesson/3E Значение арифметического выражения.py
@@ -1,8 +1,73 @@
-ex = input().split()
+ex = list(input().strip())
 
+for i in range(len(ex)):
+    while ex[i] == ' ' and ex[i] == ex[i+1]:
+        ex.pop(i+1)
+    if i == len(ex)-2:
+        break
+print(ex)
 stack = []
-for el in ex:
-    if
+postfix = []
+znak = {'-': 1, '+': 1, '*': 2}
+open = 0
+operand = ''
+flag = True
+for i in range(len(ex)):
+    if ex[i].isalpha():
+        flag = False
+        break
+    elif ex[i] == ' ':
+        if ex[i-1].isdigit() and ex[i+1].isdigit():
+            flag = False
+            break
+    elif ex[i].isdigit():
+        operand += ex[i]
+        if i == len(ex)-1 or not ex[i+1].isdigit():
+            postfix.append(int(operand))
+            operand = ''
+    else:
+        if ex[i] in ('-', '+', '*'):
+            if not ex[i-1].isdigit():
+                flag = False
+                break
+            if not stack:
+                stack.append(ex[i])
+            else:
+                while stack[-1] != '(' and znak[stack[-1]] >= znak[ex[i]]:
+                    postfix.append(stack.pop(-1))
+                stack.append(ex[i])
+        elif ex[i] == '(':
+            if ex[i-1].isdigit():
+                flag = False
+                break
+            stack.append(ex[i])
+            open += 1
+        elif ex[i] == ')':
+            if ex[i-1] in ('-', '+', "*"):
+                flag = False
+                break
+            while stack and stack[-1] != '(':
+                postfix.append(stack.pop(-1))
+            stack.pop(-1)
+            open -= 1
+        else:
+            flag = False
+            break
+
+
+print(postfix)
+print(stack)
+
+for el in reversed(stack):
+    postfix.append(el)
+
+print(postfix)
+print(flag)
+
+
+
+
+
 
 # try:
 #     print(eval(input()))
--- 3 lesson/3I Автоматизированный склад.py
+++ 3 lesson/3I Автоматизированный склад.py
@@ -1,11 +1,34 @@
 n = int(input())
 a, b = map(int, input().split())
-
-s1, s2, s3, s4 = [], [], [], []
-macros = {1: 's1.append(r)', 2: 's2.append(r)', 3: 's3.append(r)', 4: 's4.append(r)'}
+b, c = [x for x in range(1,5) if x not in (a,b)]
+rover = []
 for i in range(n):
-    r = list(map(int, input().split()))
-    r.append(i)
-    exec(macros[r[0]])
+    x, y = map(int, input().split())
+    rover.append([x, y, i])
+rover.sort(key=lambda x: x[1])
 
-print(s1, s2, s3, s4)
+queue = [''] * n
+stack = {1: [], 2: [], 3: [], 4: []}
+t, i = 0, 0
+while True:
+    t += 1
+    while i < n and rover[i][1] == t:
+        stack[rover[i][0]].append(rover[i])
+        i += 1
+    if stack[a] and stack[b]:
+        if a+2 == b or b+2 == a:
+            queue[stack[a][0][2]] = t
+            queue[stack[b][0][2]] = t
+        elif a == b+1:
+            queue[stack[b][0][2]] = t
+        elif b == a+1:
+            queue[stack[a][0][2]] = t
+    elif stack[a] and not stack[b]:
+        queue[stack[a][0][2]] = t
+    elif stack[b] and not stack[a]:
+        queue[stack[b][0][2]] = t
+
+    if i == n: break
+
+print(rover)
+print(stack)
