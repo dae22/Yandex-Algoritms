2430d6dfc9715d1307bf748dcb551b4e7f3dc9b3
dae22 <dae-22@mail.ru>
2024-11-22 15:11:22 +0300

    22/11 15:10 finish 4B

---
+++ 4 lesson/4B Родословная число потомков.py
@@ -0,0 +1,55 @@
+import sys
+
+sys.setrecursionlimit(100000)
+
+
+def tree_add(tree):
+    for i in range(1, len(tree)):
+        if tree[i][0] in mid:
+            for el in mid[tree[i][0]]:
+                tree[i].append([el])
+            del mid[tree[i][0]]
+    for i in range(1, len(tree)):
+        tree_add(tree[i])
+    return tree
+
+
+def count_tree(tree, d=0):
+    for i in range(1, len(tree)):
+        d += (1 + count_tree(tree[i]))
+    fnl.append((tree[0], d))
+    return d
+
+
+with open('input.txt', 'r') as f:
+    n = int(f.readline().strip())
+    data = [f.readline().strip().split() for _ in range(n-1)]
+
+mid = dict()
+for s, f in data:
+    if f in mid:
+        mid[f].append(s)
+    else:
+        mid[f] = [s]
+data.clear()
+
+son = []
+for el in mid.values():
+    son.extend(el)
+for el in mid.keys():
+    if el not in son:
+        FATHER = el
+        break
+son.clear()
+
+tree = [FATHER]
+for el in mid[FATHER]:
+    tree.append([el])
+del mid[FATHER]
+
+tree_add(tree)
+fnl = list()
+count_tree(tree)
+fnl.sort(key=lambda x: x[0])
+for el in fnl:
+    print(*el)
